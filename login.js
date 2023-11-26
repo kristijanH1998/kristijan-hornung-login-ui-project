7afae856f6d6bdb1b67474effdb07bda63a9d490
kristijanH1998 <kristijan.hornung1@gmail.com>
2023-11-26 09:53:53 -0800

    slide control enabled

--- login.js
+++ login.js
@@ -7,3 +7,19 @@ function validate() {
         alert("Try different username and/or password.");
     }
 }
+const formInner = document.querySelector(".form-inner");
+const loginForm = document.querySelector("form.login");
+const signupForm = document.querySelector("form.signup");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector(".signup-link a");
+signupBtn.onclick = (()=>{
+    formInner.style.flexDirection = "row-reverse";
+});
+loginBtn.onclick = (()=>{
+    formInner.style.flexDirection = "row";
+});
+signupLink.onclick = (()=>{
+    signupBtn.click();
+    return false;
+});
